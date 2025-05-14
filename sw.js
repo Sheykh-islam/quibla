5f4dc7ea14cb1a9bedf20e68c3e54eb027384bb9
Sheykh-islam <spanchpm@gmail.com>
2025-05-14 16:44:57 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,12 +6,7 @@ const urlsToCache = [
   '/index.html',
   '/manifest.json',
   // Добавьте пути к вашим иконкам
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
+  '/icons/icon-192.png',
   '/icons/icon-384x384.png',
   '/icons/icon-512x512.png'
   // Если добавите другие локальные файлы (CSS, JS) - их тоже сюда
